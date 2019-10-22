1584b34266e9ccb5a114e9adc51a7250c435a87f
nngarfdn <33192900+nngarfdn@users.noreply.github.com>
2019-10-22 10:09:31 +0700

    Update cekNomorValid.py

--- cekNomorValid.py
+++ cekNomorValid.py
@@ -1,9 +1,10 @@
 no = input()
 
 
-print(no)
-if no[0]!= '0' or no[1]!= '8' or no[2] != '1' or (len(no) == 12 and len(no) == 11) :
-    print("TIDAK VALID")
-else:
-    print ("VALID")
+
+for i in range no :
+    if no[0]!= '0' or no[1]!= '8' or no[2] != '1' or (len(no) != 12 and len(no)!= 11) or no[i] not in {0,1,2,3,4,5,6,7,8,9} :
+        print("TIDAK VALID")
+    else:
+        print ("VALID")
         
